dae19499d9f1253ba39ebded405f4220e90d3f01
Nodirjon Xamidov <107173745+XamidovNodirjon@users.noreply.github.com>
2026-03-17 15:14:48 +0500

    full update

--- README.md
+++
@@ -1,2 +0,0 @@
-# Sahovat24_Bot
-
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,23 @@ use Illuminate\Support\Facades\Route;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 Route::get('/', function () {
+    if (request()->has('lang')) {
+        $lang = request()->get('lang');
+        if (in_array($lang, ['uz', 'ru', 'en'])) {
+            session(['locale' => $lang]);
+        }
+    }
+
+    app()->setLocale(session('locale', config('app.locale')));
+
     return view('welcome');
 });
 
-Route::get('setwebhook',function(){
-    $response = Telegram::setWebhook(['url' => 'https://ab72-213-230-109-5.ngrok-free.app/api/telegram/webhook']);
+Route::get('setwebhook', function () {
+    // config('app.url') avtomatik ravishda .env dagi APP_URL ni oladi
+    $url = config('app.url') . '/api/telegram/webhook';
 
+    $response = Telegram::setWebhook(['url' => $url]);
+
+    return $response; // Natijani ekranda ko'rish uchun
 });
-
